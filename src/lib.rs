d0bbb08722cf2c46b05f85689a88cce5eda1baca
avokadoen <akselhallajs@gmail.com>
2019-09-01 22:43:05 +0200

    minor change to test

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,20 @@
 use std::collections::HashMap;
-use std::path::Path;
-use std::fs;
 use serde::{Serialize, Deserialize};
-
-#[derive(Serialize, Deserialize)]
-enum AppendType {
-    Top,
-    Bottom,
-}
 
 #[derive(Serialize, Deserialize)]
 pub struct ConfigFile {
     default_upgrade: Option<String>,
-    version_type: HashMap<String, String>,
+    version_types: HashMap<String, String>,
     version_format: String,
     changelog_paths: Vec<String>,
     categories: Vec<String>,
-    append_position: AppendType,
+    append_position: String,
 }
 
 impl ConfigFile {
-    pub fn new(path: &Path) -> Result<ConfigFile, Box<dyn std::error::Error + 'static>> {
-        let contents = fs::read_to_string(path)?;
-        let config: ConfigFile = serde_json::from_str(&contents)?;
+    pub fn new(json_string: String) -> Result<ConfigFile, Box<dyn std::error::Error + 'static>> {
+        //let contents = fs::read_to_string(path)?;
+        let config: ConfigFile = serde_json::from_str(&json_string)?;
         Ok(config)
     }
 }
--- tests/config-file-tests.rs
+++ tests/config-file-tests.rs
@@ -2,13 +2,36 @@
 mod tests {
     use super::*;
     use changelog_writer::ConfigFile;
-    use std::path::Path;
+
+    fn get_test_json_string() -> String {
+        String::from(
+        r#"{
+            "default_upgrade": "minor",
+            "version_types": [ 
+                { "major": "Ma" },
+                { "minor": "Mi" },
+                { "lesser": "Le" }
+            ],
+            "version_format": "MaMa.MiMi.LeLe",
+            "target_file_paths": [ 
+                "./something/somthing/changelog.xml", 
+                "./something/somthing2/changelog.xml",
+                "./changelog.md"
+            ],
+            "categories": [
+                "bugfix",
+                "feature",
+                "technical",
+                "tests"
+            ],
+            "append_position": "top"
+        }"#)
+    }
 
     #[test]
     fn create_new_config() {
-        let config_path = Path::new("/tests/config-file-tests.rs");
-        println!("path: {:?}", config_path);
-        let config_file = ConfigFile::new(config_path);
+        let json = get_test_json_string();
+        let config_file = ConfigFile::new(json);
         // this is probably a bad way of doing assert ...
         // alternative but not sure how to print error atm: assert!(config_file.is_ok() && !config_file.is_err());
         match config_file {
